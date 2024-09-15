b7b3cf6083e46f584301ca85cc3ceef4c0a8cb3e
unknown <2003adnanahamad@gmail.com>
2024-09-16 01:24:55 +0530

    React Practice

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Components } from "./Components";
+
+function App(){
+  return (
+    <div>
+
+    <h1>
+    This is protocal
+    </h1>
+  <Components/>
+  </div>
+  ) 
+  
+ 
+ 
+}
+export default App;
